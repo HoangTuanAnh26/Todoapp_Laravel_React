8b0e0f81db9b216c17addbed2e4576af4ea93044
hoang tuan anh <tuanhoanga789@gmail.com>
2026-06-20 09:12:27 +0900

    Add foder frontend

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Todo一覧を取得するAPI
+Route::get('/todos', [TodoController::class, 'index']); // GET /api/todos
+
+// 新しいTodoを作成するAPI
+Route::post('/todos', [TodoController::class, 'store']); // POST /api/todos
+
+// 指定したIDのTodoを削除するAPI
+Route::delete('/todos/{id}', [TodoController::class, 'destroy']); // DELETE /api/todos/{id}
